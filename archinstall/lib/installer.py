463f945f0bd440d6fd662115812c86f61715be0b
SnowsSky <exaltypixou@gmail.com>
2026-01-18 11:19:33 +0100

    Fix os-release path and ANSI_COLOR typo
    Corrected the path for os-release and fixed a typo in ANSI_COLOR.

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -942,10 +942,10 @@ class Installer:
 			self.set_hostname(hostname)
 
 		#Editing os-release to Darkarch linux
-		root = self.target if on_target else Path('/')
-		os_release = root / '/etc/os-release'
+		root = self.target
+		os_release = root / 'etc/os-release'
 		with open(os_release, 'w') as fp:
-			fp.write('NAME="DarkArch Linux"\nPRETTY_NAME="DarkArch Linux"\nID=darkarch\nID_LIKE=arch\nAINSI_COLOR="0;31"\nHOME_URL="https://github.com/darkarchlinux/"')
+			fp.write('NAME="DarkArch Linux"\nPRETTY_NAME="DarkArch Linux"\nID=darkarch\nID_LIKE=arch\nANSI_COLOR="0;31"\nHOME_URL="https://github.com/darkarchlinux/"')
 
 		if locale_config:
 			self.set_locale(locale_config)
